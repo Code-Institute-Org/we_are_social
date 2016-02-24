f014321a729816447db17181845ef5e025313349
brianojee <brian@codeinstitute.net>
2016-02-24 17:12:07 +0000

    Integrated Stripe basic payments triggered upon registration

---
+++ accounts/migrations/0002_user_stripe_id.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='user',
+            name='stripe_id',
+            field=models.CharField(default='', max_length=40),
+        ),
+    ]
