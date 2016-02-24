5784545705690a82b6dff361a2c05445d2a75d92
Aaron Sinnott <Aaron Sinnott>
2016-02-24 19:52:26 +0000

    Re-added paypal settings

--- we_are_social/settings.py
+++ we_are_social/settings.py
@@ -133,3 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SITE_URL = 'http://127.0.0.1:8000'
+PAYPAL_NOTIFY_URL = 'http://127.0.0.1/a-very-hard-to-guess-url/'
+PAYPAL_RECEIVER_EMAIL = 'aaron@codeinstitute.net'
