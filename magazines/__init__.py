b470f62dcce253fc13fd999afad8065394d1484f
Niel <nielmce@gmail.com>
2016-12-16 15:43:20 +0000

    edited init

--- magazines/__init__.py
+++ magazines/__init__.py
@@ -1,6 +1,1 @@
-from signals import subscription_was_cancelled, subscription_created
-from paypal.standard.ipn.signals import subscription_signup, subscription_cancel
 
-
-subscription_signup.connect(subscription_created)
-subscription_cancel.connect(subscription_was_cancelled)
