095fc0cca274f8a2b67486e86bceaf61d4863220
Niel <nielmce@gmail.com>
2017-01-03 14:12:33 +0000

    Commented staging imports and removed redundant settings

--- magazines/models.py
+++ magazines/models.py
@@ -4,7 +4,6 @@ from django.utils import timezone
 
 
 class Magazine(models.Model):
-
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -14,12 +13,12 @@ class Magazine(models.Model):
 
 
 class Purchase(models.Model):
-
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='purchases')
     magazine = models.ForeignKey(Magazine)
     subscription_end = models.DateTimeField(default=timezone.now())
 
 
+# To avoid an "apps not loaded" error the import from signals is placed at the bottom of file
 from signals import subscription_created, subscription_was_cancelled
 from paypal.standard.ipn.signals import valid_ipn_received
 
