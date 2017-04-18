db3d76e359466b4a80d56fa0e7ba791847ac08a6
Niel <nielmce@gmail.com>
2017-04-18 14:47:27 +0100

    Corrected errors in edit_post url, missing div in thread.html and incorrect django code in post.html

--- we_are_social/urls.py
+++ we_are_social/urls.py
@@ -26,7 +26,6 @@ from threads import views as forum_views
 from polls import api_views
 from threads import api_views as thread_api_views
 
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.get_index),
@@ -58,7 +57,7 @@ urlpatterns = [
     url(r'^new_thread/(?P<subject_id>\d+)/$', forum_views.new_thread, name='new_thread'),
     url(r'^thread/(?P<thread_id>\d+)/$', forum_views.thread, name='thread'),
     url(r'^post/new/(?P<thread_id>\d+)/$', forum_views.new_post, name='new_post'),
-    url(r'^post/edit/(?P<thread_id>\d+)/$', forum_views.edit_post, name='edit_post'),
+    url(r'^post/edit/(?P<thread_id>\d+)/(?P<post_id>\d+)/$', forum_views.edit_post, name='edit_post'),
     url(r'^post/delete/(?P<thread_id>\d+)/(?P<post_id>\d+)/$',
         forum_views.delete_post, name='delete_post'),
     url(r'^thread/vote/(?P<thread_id>\d+)/(?P<subject_id>\d+)/$',
@@ -78,6 +77,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns += [
-    url(r'^debug/', include(debug_toolbar.urls)),
+        url(r'^debug/', include(debug_toolbar.urls)),
     ]
