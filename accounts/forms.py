507d8cf4bf0980c1103aa4acfa76f3f99c69477e
Yoni Lavi <yoni@codeinstitute.net>
2017-08-25 17:11:45 +0100

    Change xrange to range, to reduce confusion
    I feel that using xrange here is a completely unnecessary performance optimisation.

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,7 +9,7 @@ class UserRegistrationForm(UserCreationForm):
         'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'
     ]
     MONTH_CHOICES = list(enumerate(MONTH_ABBREVIATIONS, 1))
-    YEAR_CHOICES = [(i, i) for i in xrange(2015, 2036)]
+    YEAR_CHOICES = [(i, i) for i in range(2015, 2036)]
 
     credit_card_number = forms.CharField(label='Credit card number')
     cvv = forms.CharField(label='Security code (CVV)')
