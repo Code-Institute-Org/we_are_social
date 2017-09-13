bff5931f05696a46529b3a206e3f12bd412c444c
Yoni Lavi <yoni@codeinstitute.net>
2017-09-13 20:03:48 +0100

    Removed weird line break

--- settings/base.py
+++ settings/base.py
@@ -62,8 +62,7 @@ ROOT_URLCONF = 'we_are_social.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
