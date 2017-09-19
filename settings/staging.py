dd18667859727b741248ed36fbb5e635d95119d6
Yoni Lavi <yoni@codeinstitute.net>
2017-09-19 17:51:12 +0100

    adding heroku hostname to ALLOWED_HOSTS

--- settings/staging.py
+++ settings/staging.py
@@ -14,6 +14,8 @@ STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_nbWefqblVg8HnYsFmp
 STRIPE_SECRET = os.getenv('STRIPE_SECRET', 'sk_test_N35jP51CRqW4FKBMa8MAL1A4')
 
 # Paypal environment variables
-SITE_URL = 'https://your-heroku-app.herokuapp.com'
 PAYPAL_NOTIFY_URL = 'https://291e2d8f.ngrok.io/a-very-hard-to-guess-url/'
 PAYPAL_RECEIVER_EMAIL = 'aaron@codeinstitute.net'
+
+SITE_URL = 'https://your-heroku-app.herokuapp.com'
+ALLOWED_HOSTS.append('your-heroku-app.herokuapp.com')
